4276f8c5f354b4324ff4480e9c1daf250ddfe785
yamani <zuful@hotmail.com>
2019-04-25 00:53:44 +0200

    Add comments.

--- index.php
+++ index.php
@@ -1,13 +1,14 @@
 <?php
-    include_once(dirname(__FILE__) . "/class/controller/MemoryController.php");
+    include_once(dirname(__FILE__) . "/class/controller/MemoryController.php"); // import de la classe controller
 
-    use memory\MemoryController;
+    use memory\MemoryController; // déclaration du namespace utilisé
 
+    // si une difficulté est renseignée alors on l'assigne sinon la difficulté "normal" est assignée par défaut
     $difficulty = (isset($_GET["difficulty"])) ? $_GET["difficulty"] : "normal";
-    $difficulty = htmlentities($difficulty);
+    $difficulty = htmlentities($difficulty); // protection contre les insertion de balise html frauduleuses
 
-    $memoryController = new MemoryController($difficulty);
-    $memoryController->saveScore(); // Sauvegarde le score si les informations nécessaires sont envoyées.
+    $memoryController = new MemoryController($difficulty); // instanciation du controller
+    $memoryController->saveScore(); // sauvegarde le score si les informations nécessaires sont envoyées.
 ?>
 <!doctype html>
 <html lang="fr">
@@ -16,21 +17,24 @@
     <title>Memory</title>
 
     <link rel="icon" type="image/x-icon" href="favicon.ico">
+    <!-- import du fichier css -->
     <link rel="stylesheet" type="text/css" href="css/style.css">
-    <link href="https://fonts.googleapis.com/css?family=Averia+Sans+Libre|Baloo+Bhaina|Righteous|Shanti|Sniglet|Voces" rel="stylesheet">
+    <!-- import des fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Righteous|Shanti|Voces" rel="stylesheet">
 </head>
 <body>
     <div id="container">
         <div id="upper">
             <h1>Memory</h1>
-            <audio controls loop="true">
+            <!-- diffuse une musique d'ambiance, les paramètres "autoplay" et "loop" permettent respectivement
+            le lancement automatique ainsi que la répétition en boucle de la musique  -->
+            <audio controls autoplay="true" loop="true">
                 <source src="<?php echo $memoryController->getSongPath(); ?>" type="audio/ogg">
-                <p>Message</p>
             </audio>
         </div>
 
         <br>
-
+        <!-- ces liens redirigent vers la page courante en y ajoutant le paramètre de difficulté -->
         <div id="difficulty">
             <h3>Difficulté : <?php echo $difficulty; ?></h3>
 
@@ -47,18 +51,24 @@
             </a>
         </div>
 
+        <!-- le contenu du tableau html est généré dynamiquement depuis le code php appelé dans la balise "table" -->
         <table>
             <?php echo $memoryController->getTableRows(); ?>
         </table>
+        <!-- ce span est utilisé par le code javascript pour afficher le chronomètre -->
         <span id="chrono"></span>
+        <!-- cette div est utilisée par le code javascript pour afficher la barre de progrès -->
         <div id="myProgress">
             <div id="myBar"></div>
         </div>
     </div>
+
+    <!-- ces informations sont récupérées et utilisées par le code en javascript -->
     <input id="numberOfPairs" type="hidden" value="<?php echo $memoryController->getNumberOfPairs(); ?>">
     <input id="index-url" type="hidden" value="<?php echo $_SERVER["PHP_SELF"]; ?>">
     <input id="difficulty-value" type="hidden" value="<?php echo $difficulty; ?>">
 
+    <!-- import de jquery et du code javascript -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
     <script src="js/script.js"></script>
 </body>
